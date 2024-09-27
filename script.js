087bb31a8068d8c4caa3640463ea04aab4baa1fb
RefOoSami001 <159632003+RefOoSami001@users.noreply.github.com>
2024-09-27 18:14:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -149,13 +149,19 @@ floating_btn.addEventListener('click', () => {
 close_btn.addEventListener('click', () => {
 	social_panel_container.classList.remove('visible')
 });
-function isMobileDevice() {
-        return /Mobi|Android/i.test(navigator.userAgent);
-    }
 
-    // Check if the user is on a mobile device
-    if (isMobileDevice()) {
-        // Prompt the user to switch to a PC
-        alert("يرجى فتح الموقع من جهاز كمبيوتر للوصول إلى المحتوى.");
-	window.close();
-    }
+jQuery(window).on('scroll', function() {
+	// stop telling people to scroll when they're scrolled
+	var window_offset = jQuery(window).scrollTop() - jQuery('body').offset().top;
+
+	if (window_offset > 10) {
+
+		jQuery('.scroll-indicator').addClass('off');
+
+	} else {
+
+		jQuery('.scroll-indicator').removeClass('off');
+
+	}
+
+});
